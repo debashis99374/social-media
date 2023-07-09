24c95ce1b7ba81a2953fa2c175ea91ff382029e4
Debashis kar <debashis99374@gmail.com>
2023-07-09 19:44:35 +0530

    added password functionality to signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,11 +1,12 @@
 import {useState,useEffect,useContext} from 'react'
 import { AuthContext } from '../context/authContext'
 import { useNavigate,Link } from 'react-router-dom'
-
+import { BiShow,BiHide } from "react-icons/bi";
 import "../css/signUp.css"
 
 
 export default function Signup(){
+    const [showPassword,setShowPassword]=useState(false)
     const {signupHandler,token}=useContext(AuthContext)
     const navigate=useNavigate()
     const [signupForm,setSignupForm]=useState({
@@ -35,7 +36,13 @@ export default function Signup(){
             <lable>Username:</lable> 
             <input type='text' value={signupForm.username} onChange={(e)=>setSignupForm({...signupForm,username:e.target.value})} />
             <lable>Password:</lable> 
-            <input type='password' value={signupForm.password} onChange={(e)=>setSignupForm({...signupForm,password:e.target.value})} />
+            <div className="password-input">
+            <input type={showPassword?"text":"password"} style={{width:"95%",height:"18px"}} value={signupForm.password} onChange={(e)=>setSignupForm({...signupForm,password:e.target.value})} />
+             <p onClick={()=>setShowPassword(!showPassword)}>
+               {showPassword?<BiHide/>:<BiShow/>}
+             </p>
+            </div>
+           
             <button type='submit'>Submit</button>
         </form>
         <p>Already a user? <Link to='/login'>Log In</Link> </p>
